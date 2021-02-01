1fec7c8ec690421a69051dfe948f96fbc520095b
Ubuntu <ubuntu@ip-172-31-22-129.ec2.internal>
2021-02-01 10:43:30 +0000

    user login1

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
-        <a class="navbar-brand" href="/">CatLover</a>
+        <a class="navbar-brand" href="/">Microposts</a>
          
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -9,10 +9,21 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
-                    
         </div>
     </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the CatLover</h1>
